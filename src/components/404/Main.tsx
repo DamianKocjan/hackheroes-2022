bc9a83ac8af3317bf3fcf4841d20c934fecb26b8
Damian Kocjan <DamianoKPL@wp.pl>
2022-10-23 18:51:43 +0200

    fix styles

--- src/components/404/Main.tsx
+++ src/components/404/Main.tsx
@@ -16,11 +16,8 @@ export const Main: React.FC = () => (
           Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
         <div className="mt-6">
-          <Link
-            href="/"
-            className="text-base font-medium text-indigo-600 hover:text-indigo-500"
-          >
-            <a>
+          <Link href="/">
+            <a className="text-base font-medium text-indigo-600 hover:text-indigo-500">
               Go back home<span aria-hidden="true"> &rarr;</span>
             </a>
           </Link>
